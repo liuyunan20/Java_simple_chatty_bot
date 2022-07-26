514b86c9888265a0516caa90a5eb8e5f5d0269d4
liuyunan <liuyunan.20@gmail.com>
2022-07-26 15:31:01 -0700

    Write a program that reads five words from the standard input and outputs each word in a new line. The words should be in the same order.
    Sample Input:
    This Java platform
    is adaptive
    
    Sample Output:
    This
    Java
    platform
    is
    adaptive

---
+++ Topics/Scanning the input/Printing each word in a new line/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String st1, st2, st3, st4, st5, st;
+        st1 = scanner.next();
+        st2 = scanner.next();
+        st3 = scanner.next();
+        st4 = scanner.next();
+        st5 = scanner.next();
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(st4);
+        System.out.println(st5);
+    }
+}
