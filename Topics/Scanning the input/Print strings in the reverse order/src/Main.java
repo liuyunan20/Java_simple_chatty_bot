1f3c4912ad7a004eb6148a6fed28cdc10aa71a25
liuyunan <liuyunan.20@gmail.com>
2022-07-26 15:17:33 -0700

    Print strings in the reverse order Write a program that reads three strings and outputs them in the reverse order, each in a new line. Sample Input: Java Programming Language
    Sample Output:
    Language
    Programming
    Java

---
+++ Topics/Scanning the input/Print strings in the reverse order/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String st1 = scanner.nextLine();
+        String st2 = scanner.nextLine();
+        String st3 = scanner.nextLine();
+        System.out.println(st3);
+        System.out.println(st2);
+        System.out.println(st1);
+    }
+}
