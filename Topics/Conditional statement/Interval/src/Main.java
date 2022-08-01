549ae5fcf33d27b319fb7c375de353ff16362d5d
liuyunan <liuyunan.20@gmail.com>
2022-08-01 14:46:23 -0700

    Interval Given an integer as an input, print True if its value falls within the interval (−15,12]∪(14,17)∪[19,+∞). Otherwise, print False (case sensitive).
    Notes:
    
    numbers denoted with a parenthesis () are exclusive;
    numbers denoted with a square bracket [] are inclusive.

---
+++ Topics/Conditional statement/Interval/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if ((-15 < n && n <= 12) || (14 < n && n < 17) || (19 <= n)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
